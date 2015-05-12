2de4a83cc8b03877fbdcd6bd39881b190eacfc09
Vladyslav Bondarenko <vladyslavbond@protonmail.ch>
2015-05-12 18:20:48 +0200

    Resolves Wbondar/vedabase00#6

--- js/popups.js
+++ js/popups.js
@@ -8,8 +8,11 @@
           // Setup page.
         jQuery('body').append("<div id='popup' class='hidden'><p>Placeholder.</p></div>");
         popUp = jQuery('#popup');
+
         jQuery('body').append("<a id='share' href='#'>Share.</a>");
         shareAnchor = jQuery('#share');
+
+        doneAnchor = jQuery("<a id='done' href='#done'>Done.</a>");
 
           // Define functions.
         var hide = function (objectToHide)
@@ -22,16 +25,30 @@
           objectToShow.removeClass('hidden');
         }
 
-        var lastHightlightedObject;
-        var hightlight = function (objectToHightlight)
+        var selectedParagraph;
+        var deselect = function (objectToDeselect)
         {
-            if (lastHightlightedObject != null)
-            {
-              lastHightlightedObject.removeClass('highlighted');
-            }
-            objectToHightlight.addClass('highlighted');
-            lastHightlightedObject = objectToHightlight;
+          objectToDeselect.removeClass('highlighted');
         };
+        var select = function (objectToSelect)
+        {
+
+            if (selectedParagraph != null)
+            {
+              deselect(selectedParagraph);
+            }
+            objectToSelect.addClass('highlighted');
+            selectedParagraph = objectToSelect;
+        };
+        var toggleSelection = function (objectToToggleSelection, isSelected)
+        {
+          if (isSelected)
+          {
+            select(objectToToggleSelection);
+          } else {
+            deselect(objectToToggleSelection);
+          }
+        }
 
           // Define event handlers.
         shareAnchor.on
@@ -44,15 +61,43 @@
             var paragraphId = jQuery(this).attr('href');
             var paragraph = jQuery(paragraphId);
 
-            hightlight(paragraph);
+            select(paragraph);
 
             show(popUp);
 
-            popUp.contents( ).replaceWith("<p>Content of the pop-up for the paragraph with id of " + paragraphId + ".</p>");
+            /* Move anchor to different location
+             * in order to keep it's event handler alive
+             * when content of the pop-up will be cleared.
+             */
+            $('body').after(doneAnchor);
+            hide(doneAnchor);
+
+            /* Remove content of the pop-up,
+             * in order to avoid duplications.
+             */
+            popUp.contents( ).replaceWith("");
+
+            /* Generate content of the pop-up.
+             */
+            popUp.append("<p>Content of the pop-up for the paragraph with id of " + paragraphId + ".</p>");
+            show(doneAnchor);
+            popUp.append(doneAnchor);
             paragraph.after(popUp);
 
             return false;
           }
+        );
+
+        doneAnchor.on
+        (
+            'click'
+          , function ( )
+            {
+              deselect(selectedParagraph);
+              hide(popUp);
+
+              return false;
+            }
         );
 
         jQuery('div.record').hover
